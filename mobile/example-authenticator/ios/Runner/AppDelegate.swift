64a4ce76f6fe0fb661f8676fcd03a3ac7fe24de1
sri <sriyulianti@gmail.com>
2025-06-04 09:34:18 +0700

    Add ios flutter channel

--- mobile/example-authenticator/ios/Runner/AppDelegate.swift
+++ mobile/example-authenticator/ios/Runner/AppDelegate.swift
@@ -3,11 +3,37 @@ import Flutter
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
+  var initialLink: URL?
+
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
+    if let url = launchOptions?[.url] as? URL {
+      initialLink = url
+    }
+
+    let controller = window?.rootViewController as! FlutterViewController
+    let channel = FlutterMethodChannel(name: "app.linking.channel", binaryMessenger: controller.binaryMessenger)
+
+    channel.setMethodCallHandler { [weak self] (call, result) in 
+      if call.method == "getLaunchUri" {
+        result(self?.initialLink?.absoluteString)
+      } else {
+        result(FlutterMethodNotImplemented)
+      }
+    }
+
+    // GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+
+  override func application(
+    _ app: UIApplication,
+    open url: URL,
+    options: [UIApplication.OpenURLOptionsKey : Any] = [:]
+  ) -> Bool {
+    initialLink = url
+    return true
   }
 }
